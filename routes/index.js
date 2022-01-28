dcee925489c1516aefef161afe5a548e6924dbe0
Yurii <juracherniak@gmail.com>
2022-01-28 14:54:54 +0200

    start route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const async = require("async");
+
+const User = require("../models/user");
+const Message = require("../models/message");
+
+
+let path = require("path");
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get("/sign-up", function (req, res, next) {
+    res.render("sign-up-form", { title: "Sign Up" });
+});
+
+router.post("/sign-up", function (req, res, next) {
+    res.render("sign-up-form", { title: "Sign Up" });
 });
 
 module.exports = router;
